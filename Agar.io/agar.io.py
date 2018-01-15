97a2ecada9be465df5cd38a66a0c6eaf60925d7d
matvey19-meet <matvey19@meet.mit.edu>
2018-01-15 19:19:53 +0200

    DONE

---
+++ Agar.io/agar.io.py
@@ -0,0 +1,24 @@
+import turtle
+import time
+import random
+from ball import Ball 
+
+turtle.tracer(0)
+hideturtle.turtle()
+
+SLEEP = 0.0077
+SCREEN_WIDTH= turtle.getcanvas().winfo_width()/2
+SCREEN_HEIGHT= turtle.getcanvas().winfo_height()/2
+RUNNING= True 
+
+
+
+NUMBER_OF_BALLS = 5
+MINIMUM_BALL_RADIUS = 10
+MAXIMUM_BALL_RADIUS = 100
+MINIMUM_BALL_DX to = -5
+MAXIMUM_BALL_DX = 5
+MINIMUM_BALL_DY = -5
+MAXIMUM_BALL_DY = 5
+
+
--- Agar.io/ball.py
+++ Agar.io/ball.py
@@ -1,6 +1,8 @@
 import turtle
 from turtle import *
 
+
+
 class Ball(Turtle):
 	def __init__(self, x, y, dx, dy, r, color):
 		Turtle.__init__(self)
@@ -14,23 +16,36 @@ class Ball(Turtle):
 		self.shapesize = self.r/10
 		self.color(color)
 	def move(self, width, height):
-		
-		current_x =self.xcor()
-		new_x=self.xcor()+self.dx
-		current_y= self.ycor()
-		new_y=self.ycor()+self.dy
-		right_side_ball= new_x+r
-		left_side_ball=new_x-r
-		top_side_ball=new_y+r
-		top_side_ball=new_y-r
-		
+		self.width=width
+		self.height=height
+		current_x=self.xcor()
+		new_x=current_x+self.dx
+		current_y=self.ycor()
+		new_y=current_y+self.dy
+
+		right_side_ball= new_x + self.r
+		left_side_ball= new_x - self.r
+		top_side_ball= new_y + self.r
+		bottom_side_ball= new_y - self.r
+
+
+		if top_side_ball>= height/2:
+			new_y=current_y
+		if bottom_side_ball <= -(height/2):
+			new_y=current_y
+		if right_side_ball <= width/2:
+			new_x=current_x
+		if left_side_ball >= -(width/2):
+			new_x=current_x
+
 
 
 		self.goto(new_x,new_y)
 
-ball1=Ball(0,0,0,2,100,"red")
-ball1.goto(0,50)
-ball1.move(100,100)
+ball1=Ball(0,0,1,0,100,"red")
+# ball1.goto(0,100)
+while True:
+	ball1.move(100,100)
 
 turtle.mainloop()
 
